3e6b206ebeed9cd2a654b92622d8569b5b3d1883
MaxDrechsler <max.drechsler@epitech.eu>
2022-12-02 03:09:24 +0100

    movais dossier

---
+++ movais/movais.js
@@ -0,0 +1,11 @@
+function changeColor() {
+    const counter = Math.random;
+    if (counter == 1) {
+        document.body.style.backgroundColor = "green";
+        counter++;
+    } else {
+        document.body.style.backgroundColor = "red";
+        counter++;
+    }
+    counter++;
+}
