689cfbcb9dbaa381fc650061e8fd41374bca39c2
MaxDrechsler <max.drechsler@epitech.eu>
2022-12-02 03:39:01 +0100

    ya le lien wila

--- movais/movais.js
+++ movais/movais.js
@@ -1,9 +1,10 @@
 function changeColor() {
-    const nb = Math.random;
+    const nb = Math.random();
     if (nb == 1) {
         document.body.style.backgroundColor = "green";
     }
     if (nb == 0) {
         document.body.style.backgroundColor = "red";
     }
+    document.body.style.backgroundColor = "red";
 }
